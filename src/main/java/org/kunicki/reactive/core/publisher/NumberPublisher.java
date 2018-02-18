be0bcaa776898d00461205dacc128486c2e050ae
Jacek Kunicki <jacek.kunicki@softwaremill.com>
2018-02-18 17:58:21 +0100

    Number publisher

--- src/main/java/org/kunicki/reactive/core/publisher/NumberPublisher.java
+++ src/main/java/org/kunicki/reactive/core/publisher/NumberPublisher.java
@@ -1,5 +1,30 @@
 package org.kunicki.reactive.core.publisher;
 
-public class NumberPublisher {
+import java.util.concurrent.Flow;
+import java.util.concurrent.SubmissionPublisher;
+import java.util.stream.IntStream;
 
+public class NumberPublisher implements Flow.Publisher<Integer> {
+
+    private final SubmissionPublisher<Integer> submissionPublisher = new SubmissionPublisher<>();
+
+    public void start() {
+        IntStream.iterate(1, i -> i+ 1).forEach(i -> {
+            submissionPublisher.submit(i);
+            sleep();
+        });
+    }
+
+    private void sleep() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void subscribe(Flow.Subscriber<? super Integer> subscriber) {
+        submissionPublisher.subscribe(subscriber);
+    }
 }
