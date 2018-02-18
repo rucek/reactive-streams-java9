5efb9909e589239e19a06060f58ade485980fccc
Jacek Kunicki <jacek.kunicki@softwaremill.com>
2018-02-18 18:43:21 +0100

    Mapping processor

--- src/main/java/org/kunicki/reactive/core/processor/MappingProcessor.java
+++ src/main/java/org/kunicki/reactive/core/processor/MappingProcessor.java
@@ -1,5 +1,18 @@
 package org.kunicki.reactive.core.processor;
 
-public class MappingProcessor<In, Out> {
+import java.util.function.Function;
 
+public class MappingProcessor<In, Out> extends ProcessorBase<In, Out> {
+
+    private final Function<In, Out> function;
+
+    public MappingProcessor(Function<In, Out> function) {
+        this.function = function;
+    }
+
+    @Override
+    public void onNext(In item) {
+        submit(function.apply(item));
+        subscription.request(1);
+    }
 }
