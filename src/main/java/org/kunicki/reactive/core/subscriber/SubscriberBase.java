43b71dedf263a2b840f65686e7358c0849026cdc
Jacek Kunicki <jacek.kunicki@softwaremill.com>
2018-02-18 18:03:27 +0100

    Subscriber base

--- src/main/java/org/kunicki/reactive/core/subscriber/SubscriberBase.java
+++ src/main/java/org/kunicki/reactive/core/subscriber/SubscriberBase.java
@@ -1,5 +1,23 @@
 package org.kunicki.reactive.core.subscriber;
 
-public abstract class SubscriberBase<T> {
+import org.kunicki.reactive.util.SimpleLogging;
 
+import java.util.concurrent.Flow;
+
+public abstract class SubscriberBase<T> implements Flow.Subscriber<T>, SimpleLogging {
+
+    protected Flow.Subscription subscription;
+
+    @Override
+    public void onSubscribe(Flow.Subscription subscription) {
+        info("onSubscribe");
+
+        // https://github.com/reactive-streams/reactive-streams-jvm#2.5
+        if (this.subscription == null) {
+            this.subscription = subscription;
+            this.subscription.request(1);
+        } else {
+            subscription.cancel();
+        }
+    }
 }
