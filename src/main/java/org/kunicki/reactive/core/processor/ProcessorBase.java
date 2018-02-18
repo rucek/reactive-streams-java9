b794dfd4f8d8bf9f1698604156e623640c0cc8aa
Jacek Kunicki <jacek.kunicki@softwaremill.com>
2018-02-18 18:38:34 +0100

    Processor base

--- src/main/java/org/kunicki/reactive/core/processor/ProcessorBase.java
+++ src/main/java/org/kunicki/reactive/core/processor/ProcessorBase.java
@@ -1,5 +1,30 @@
 package org.kunicki.reactive.core.processor;
 
-public abstract class ProcessorBase<In, Out> {
+import org.kunicki.reactive.core.subscriber.SubscriberBase;
 
+import java.util.concurrent.Flow;
+import java.util.concurrent.SubmissionPublisher;
+
+public abstract class ProcessorBase<In, Out> extends SubscriberBase<In> implements Flow.Publisher<Out> {
+
+    private final SubmissionPublisher<Out> submissionPublisher = new SubmissionPublisher<>();
+
+    @Override
+    public void subscribe(Flow.Subscriber<? super Out> subscriber) {
+        submissionPublisher.subscribe(subscriber);
+    }
+
+    @Override
+    public void onError(Throwable throwable) {
+        submissionPublisher.closeExceptionally(throwable);
+    }
+
+    @Override
+    public void onComplete() {
+        submissionPublisher.close();
+    }
+
+    void submit(Out item) {
+        submissionPublisher.submit(item);
+    }
 }
