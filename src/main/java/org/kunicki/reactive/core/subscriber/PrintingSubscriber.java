41f68eb0c40229ff1c72cf98be25608cc82ebe09
Jacek Kunicki <jacek.kunicki@softwaremill.com>
2018-02-18 18:34:07 +0100

    Printing subscriber

--- src/main/java/org/kunicki/reactive/core/subscriber/PrintingSubscriber.java
+++ src/main/java/org/kunicki/reactive/core/subscriber/PrintingSubscriber.java
@@ -1,5 +1,22 @@
 package org.kunicki.reactive.core.subscriber;
 
-public class PrintingSubscriber<T> {
+import org.kunicki.reactive.util.SimpleLogging;
 
+public class PrintingSubscriber<T> extends SubscriberBase<T> implements SimpleLogging {
+
+    @Override
+    public void onNext(T item) {
+        info("item: " + item);
+        subscription.request(1);
+    }
+
+    @Override
+    public void onError(Throwable throwable) {
+        error(throwable.getMessage());
+    }
+
+    @Override
+    public void onComplete() {
+        info("onComplete");
+    }
 }
