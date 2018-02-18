d5f0e21eb3af84254803c3efbe7eb534d95667b4
Jacek Kunicki <jacek.kunicki@softwaremill.com>
2018-02-18 18:46:56 +0100

    Core demo

--- src/main/java/org/kunicki/reactive/core/CoreApp.java
+++ src/main/java/org/kunicki/reactive/core/CoreApp.java
@@ -1,5 +1,22 @@
 package org.kunicki.reactive.core;
 
+import org.kunicki.reactive.core.processor.FilteringProcessor;
+import org.kunicki.reactive.core.processor.MappingProcessor;
+import org.kunicki.reactive.core.publisher.NumberPublisher;
+import org.kunicki.reactive.core.subscriber.PrintingSubscriber;
+
 public class CoreApp {
 
+    public static void main(String[] args) {
+        NumberPublisher numberPublisher = new NumberPublisher();
+        MappingProcessor<Integer, Integer> mappingProcessor = new MappingProcessor<>(i -> i + 1);
+        FilteringProcessor<Integer> filteringProcessor = new FilteringProcessor<>(i -> i % 2 == 0);
+        PrintingSubscriber<Integer> printingSubscriber = new PrintingSubscriber<>();
+
+        numberPublisher.subscribe(mappingProcessor);
+        mappingProcessor.subscribe(filteringProcessor);
+        filteringProcessor.subscribe(printingSubscriber);
+
+        numberPublisher.start();
+    }
 }
