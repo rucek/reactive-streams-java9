4176d16b41b6993ddc485cad7a0830e8fdde7129
Jacek Kunicki <jacek.kunicki@softwaremill.com>
2018-05-07 21:27:30 +0200

    Termination handling within subscription

--- src/main/java/org/kunicki/reactive/dyi/SimplePublisher.java
+++ src/main/java/org/kunicki/reactive/dyi/SimplePublisher.java
@@ -2,6 +2,7 @@ package org.kunicki.reactive.dyi;
 
 import java.util.Iterator;
 import java.util.concurrent.Flow;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.stream.IntStream;
 
 public class SimplePublisher implements Flow.Publisher<Integer> {
@@ -44,6 +45,7 @@ public class SimplePublisher implements Flow.Publisher<Integer> {
     private class SimpleSubscription implements Flow.Subscription {
 
         private final Flow.Subscriber<? super Integer> subscriber;
+        private final AtomicBoolean terminated = new AtomicBoolean(false);
 
         public SimpleSubscription(Flow.Subscriber<? super Integer> subscriber) {
             this.subscriber = subscriber;
@@ -55,18 +57,18 @@ public class SimplePublisher implements Flow.Publisher<Integer> {
                 subscriber.onError(new IllegalArgumentException());
             }
 
-            for (long demand = n; demand > 0 && iterator.hasNext(); demand--) {
+            for (long demand = n; demand > 0 && iterator.hasNext() && !terminated.get(); demand--) {
                 subscriber.onNext(iterator.next());
             }
 
-            if (!iterator.hasNext()) {
+            if (!iterator.hasNext() && !terminated.getAndSet(true)) {
                 subscriber.onComplete();
             }
         }
 
         @Override
         public void cancel() {
-
+            terminated.set(true);
         }
     }
 }
